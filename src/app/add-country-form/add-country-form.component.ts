adfb2e0fc8b995054adce05e5956c0f3fbb37186
Seb <developer.seb.v@gmail.com>
2025-06-21 17:50:10 -0700

    added re-direct

--- src/app/add-country-form/add-country-form.component.ts
+++ src/app/add-country-form/add-country-form.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-country-form',
   templateUrl: './add-country-form.component.html',
   styleUrls: ['./add-country-form.component.css'],
 })
 export class AddCountryFormComponent implements OnInit {
-  constructor(private fb: FormBuilder, private http: HttpClient) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.http
@@ -39,11 +44,15 @@ export class AddCountryFormComponent implements OnInit {
         .subscribe((res) => {
           alert('Country added');
 
-          console.log(res);
+          console.log(res, this.GoToAddPlayer());
           // this.goToAddPlayer()
         });
     } else {
       alert('Please fill out fields');
     }
+  }
+
+  GoToAddPlayer() {
+    this.router.navigate(['add-player']);
   }
 }
