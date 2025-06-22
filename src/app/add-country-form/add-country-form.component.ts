736520f3d7e44fd456055eb894ccfad7afb75c93
Seb <developer.seb.v@gmail.com>
2025-06-21 17:45:34 -0700

    country form working again

--- src/app/add-country-form/add-country-form.component.ts
+++ src/app/add-country-form/add-country-form.component.ts
@@ -1,24 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-add-country-form',
   templateUrl: './add-country-form.component.html',
-  styleUrls: ['./add-country-form.component.css']
+  styleUrls: ['./add-country-form.component.css'],
 })
-export class AddCountryFormComponent {
-  
-  constructor(private fb: FormBuilder) {
-    
+export class AddCountryFormComponent implements OnInit {
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:5151/api/player/country')
+      .subscribe((response) => {
+        this.countries = response;
+      });
   }
 
+  countryForm = this.fb.group({
+    countryId: ['', Validators.minLength(1)],
+    countryName: ['', Validators.required],
+  });
 
-   playerForm = this.fb.group({
-      countryId: ['', Validators.minLength(1)],
-      countryName: ['', Validators.required],
-    })
+  header: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'Application/Json',
+  });
 
+  countries: any;
 
+  onSubmit() {
+    if (this.countryForm.valid) {
+      this.http
+        .post(
+          'http://localhost:5151/api/player/addcountry',
+          this.countryForm.value,
+          { headers: this.header }
+        )
+        .subscribe((res) => {
+          alert('Country added');
 
-    
+          console.log(res);
+          // this.goToAddPlayer()
+        });
+    } else {
+      alert('Please fill out fields');
+    }
+  }
 }
